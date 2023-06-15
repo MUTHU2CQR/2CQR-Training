ff3a0b23ee37c97a6a83728a14aba3ee9d25e82b
MUTHU2CQR <136307764+MUTHU2CQR@users.noreply.github.com>
2023-06-15 14:07:46 +0530

    last commit 15-06-2023

---
+++ sample3/sample3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sample3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string  Name;
+            string messagetext;
+
+            messagetext = "your name is:";
+
+            Name = textBox1.Text;
+            MessageBox.Show(messagetext+"--->"+Name);
+
+        }
+    }
+}
