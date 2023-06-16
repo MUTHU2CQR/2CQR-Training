26cd1d94bdf984f1fac2089c32b466eb8222db24
MUTHU2CQR <136307764+MUTHU2CQR@users.noreply.github.com>
2023-06-16 10:32:42 +0530

    last commit 16-06-2023
    if else condition code

---
+++ sample7/sample7/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace sample7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSUB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxname = new System.Windows.Forms.TextBox();
+            this.textBoxph = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSUB
+            // 
+            this.buttonSUB.Location = new System.Drawing.Point(369, 298);
+            this.buttonSUB.Name = "buttonSUB";
+            this.buttonSUB.Size = new System.Drawing.Size(85, 42);
+            this.buttonSUB.TabIndex = 0;
+            this.buttonSUB.Text = "SUBMIT";
+            this.buttonSUB.UseVisualStyleBackColor = true;
+            this.buttonSUB.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NAME";
+            // 
+            // textBoxname
+            // 
+            this.textBoxname.Location = new System.Drawing.Point(250, 131);
+            this.textBoxname.Name = "textBoxname";
+            this.textBoxname.Size = new System.Drawing.Size(204, 26);
+            this.textBoxname.TabIndex = 2;
+            this.textBoxname.TextChanged += new System.EventHandler(this.textBoxname_TextChanged);
+            // 
+            // textBoxph
+            // 
+            this.textBoxph.Location = new System.Drawing.Point(250, 205);
+            this.textBoxph.Name = "textBoxph";
+            this.textBoxph.Size = new System.Drawing.Size(204, 26);
+            this.textBoxph.TabIndex = 3;
+            this.textBoxph.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PH NO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 476);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxph);
+            this.Controls.Add(this.textBoxname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSUB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSUB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxname;
+        private System.Windows.Forms.TextBox textBoxph;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ sample7/sample7/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sample7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxname.Text = "";
+            if (textBoxname.Text == "")
+            {
+                MessageBox.Show("NAME--> MUTHU \n PH NO-->99876543123", "WARNING", MessageBoxButtons.OK);
+                
+            }
+           
+            else
+            {
+                MessageBox.Show("please enter your name");
+                MessageBox.Show("please enter your phone number");
+            }
+        }
+
+        private void textBoxname_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
