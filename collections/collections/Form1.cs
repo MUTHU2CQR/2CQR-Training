8a45b0360b805dba009ea4bafb028abce27999d2
MUTHU2CQR <136307764+MUTHU2CQR@users.noreply.github.com>
2023-06-20 15:37:06 +0530

    Last commit 20-06-2023
    list

---
+++ collections/collections/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace collections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List < string > students = new List<string>();
+            students.Add("BOYSSSS");
+            students.Add("-----------------------------------");
+            students.Add("1---muthu");
+            students.Add("2---sanjay");
+            students.Add("3---vicky");
+            students.Add("4---abdul");
+
+            for (int i = 0; i < students.Count; i++)
+            {
+
+                listBox1.Items.Add(students[i]);
+
+            }
+        }
+    }
+}
