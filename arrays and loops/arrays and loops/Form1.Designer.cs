fc541ff5328bac9f0e917d9d12777e3a14cc17c8
MUTHU2CQR <136307764+MUTHU2CQR@users.noreply.github.com>
2023-06-20 13:28:48 +0530

    last commit 20-06-2023
    time tables using loops

--- arrays and loops/arrays and loops/Form1.Designer.cs
+++ arrays and loops/arrays and loops/Form1.Designer.cs
@@ -30,13 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(48, 43);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 31);
+            this.button1.Size = new System.Drawing.Size(160, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "ARRAYS";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,21 +49,53 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(214, 43);
+            this.listBox1.Location = new System.Drawing.Point(255, 43);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(183, 104);
             this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "array size ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "5";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Run Times";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 244);
+            this.ClientSize = new System.Drawing.Size(632, 397);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +103,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- arrays and loops/arrays and loops/Form1.cs
+++ arrays and loops/arrays and loops/Form1.cs
@@ -23,13 +23,45 @@ namespace arrays_and_loops
             int[]  lottery_number;
             lottery_number = new int [49];
             
-            for (int i = 0; i < lottery_number.Length; i++)
+            for (int i = 0; i< lottery_number.Length; i++)
             {
-                lottery_number[i] = i + 0;
+                 lottery_number[i] = i + 1;
                 listBox1.Items.Add(lottery_number[i]);
 
             }
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+
+            int[] arraySize;
+            arraySize = new int[a];
+
+
+            for (int i = 0; i != (arraySize.Length); i++)
+            {
+
+                arraySize[i] = i + 1;
+
+                listBox1.Items.Add(arraySize[i]);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int[] runtimes;
+            runtimes = new int[10];
+            int times = int.Parse(textBox1.Text);
+
+            for(int i = 1; i != runtimes.Length; i++)
+            {
+                runtimes[i] = i * times;
+                listBox1.Items.Add(times + "times" + i + "=" + runtimes[i]);
+            }
         }
     }
 }
