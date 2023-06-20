c84f6764b626a7797b969e4e157e0e4da062aee4
MUTHU2CQR <136307764+MUTHU2CQR@users.noreply.github.com>
2023-06-20 12:29:19 +0530

    last commit 20-06-2023
    arrrays print using loop

---
+++ arrays and loops/arrays and loops/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrays_and_loops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int[]  lottery_number;
+            lottery_number = new int [49];
+            
+            for (int i = 0; i < lottery_number.Length; i++)
+            {
+                lottery_number[i] = i + 0;
+                listBox1.Items.Add(lottery_number[i]);
+
+            }
+
+        }
+    }
+}
